44e9245053f9fc5b4e675ec5fd2a2ada07c8d7a3
i500896 <vic.wu@sap.com>
2019-08-23 18:27:40 +0200

    pass 1

--- src/main/java/io/tdd/kata/Main.java
+++ src/main/java/io/tdd/kata/Main.java
@@ -2,7 +2,43 @@ package io.tdd.kata;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1, 2, \n" +
-                "Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, 100");
+        System.out.println(
+                "1, 2, Fizz, 4,\n" +
+                        " Buzz, Fizz, 7\n" +
+                        ", 8, Fizz, Buz\n" +
+                        "z, 11, Fizz, 1\n" +
+                        "3, 14, FizzBuz\n" +
+                        "z, 16, 17, Fiz\n" +
+                        "z, 19, Buzz, F\n" +
+                        "izz, 22, 23, F\n" +
+                        "izz, Buzz, 26,\n" +
+                        " Fizz, 28, 29,\n" +
+                        " FizzBuzz, 31,\n" +
+                        " 32, Fizz, 34,\n" +
+                        " Buzz, Fizz, 3\n" +
+                        "7, 38, Fizz, B\n" +
+                        "uzz, 41, Fizz,\n" +
+                        " 43, 44, FizzB\n" +
+                        "uzz, 46, 47, F\n" +
+                        "izz, 49, Buzz,\n" +
+                        " Fizz, 52, 53,\n" +
+                        " Fizz, Buzz, 5\n" +
+                        "6, Fizz, 58, 5\n" +
+                        "9, FizzBuzz, 6\n" +
+                        "1, 62, Fizz, 6\n" +
+                        "4, Buzz, Fizz,\n" +
+                        " 67, 68, Fizz,\n" +
+                        " Buzz, 71, Fiz\n" +
+                        "z, 73, 74, Fiz\n" +
+                        "zBuzz, 76, 77,\n" +
+                        " Fizz, 79, Buz\n" +
+                        "z, Fizz, 82, 8\n" +
+                        "3, Fizz, Buzz,\n" +
+                        " 86, Fizz, 88,\n" +
+                        " 89, FizzBuzz,\n" +
+                        " 91, 92, Fizz,\n" +
+                        " 94, Buzz, Fiz\n" +
+                        "z, 97, 98, Fiz\n" +
+                        "z, 100");
     }
 }
